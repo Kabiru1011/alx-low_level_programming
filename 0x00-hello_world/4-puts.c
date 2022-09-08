27d422518c9e2e4ab54dbaba675e16ff4647eeb1
Kabiru1011 <kabiruibrahim2000@gail.com>
2022-09-08 15:23:00 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inlude <stdio.h>
+
+/**
+ *main - print the string in the putfunction
+ *
+ * descriptino: using the main function 
+ * this program prints "programming is like building a multilingualpuzzle */
+{
+	puts("\"programmig is like building a multilingual puzzzle");
+	return9(0);
+}
