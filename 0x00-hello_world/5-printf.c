35f13bed53962c44f9b9d0046b52c541090dbfe1
Kabiru1011 <kabiruibrahim2000@gail.com>
2022-09-10 05:28:48 -0700

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the puts function
+ *
+ * Description: using the main function 
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+ int main(void)
+{
+	printf("with proper programmer, but the outcome is piece of art,\n");
+	return (0);
+}
