f1a3d9f678c62738f3fd5c5e6e9cbc907ab3f9b9
Kabiru1011 <kabiruibrahim2000@gail.com>
2022-09-14 18:56:19 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,20 @@
 /**
 *main - prints_putchar, followed by a new line
  * Return: 0
- * /
+ */
+
 int main(void)
 {
-_putchar('_');
-_putchar('p'); 
-_putchar('u'); 
-_putchar('t'); 
-_putchar('c'); 
-_putchar('h'); 
-_putchar('a'); 
-_putchar('r'); 
-_putchar('\n');
 
-return (0);
+	_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+
+		ruturn (0);
 }
-
