83e7f5936371e58ffcf5065ef7f782909eda00fa
Kabiru1011 <kabiruibrahim2000@gail.com>
2022-09-15 14:51:04 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - print the string in the puts function
  *
- * Description: using the main function 
+ * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper programmer, but the outcome is piece of art,\n");
+	printf("with proper grammar, but the outcome is piece of art,\n");
 
 	return (0);
 }
