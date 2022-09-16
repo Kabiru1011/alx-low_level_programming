8bce570d8b3d4e009d004205d30a1b5aad4ebc4d
Kabiru1011 <kabiruibrahim2000@gail.com>
2022-09-16 16:26:37 -0700

    8-print_square.c

---
+++ 8-print_square.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <main.h>
+/**
+ *print_square - print squres
+ *@size: parameter
+ *return: returns nothing
+ */
+
+void print_square(int size)
+{
+	int i,j;
+	if (size <= 0>)
+			_putchar(10);
+			for(i = 0, i < size; i+)
+			{
+			for (j = 0, j < size; j++)
+			{
+				putcha('#')
+			}
+			putchar(\n)
+			}
+}
