6749ab26a3612c66732b65cfcbee4eea42613f1a
Kabiru1011 <kabiruibrahim2000@gail.com>
2022-09-20 15:00:52 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Muhd junior
+ * Des: Header contain prototype fot all functions
+ * used in 0x04-more_functions_nested_loops_directory
+ */
+
+int _putchar(char c);
